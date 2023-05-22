a26dc3c923d91cc3dcbd7281e1799b3eea745886
yusuftogtay <yusuf.togtay@gmail.com>
2023-05-22 14:31:33 +0300

    Add locale

---
+++ locale/tr/auth.php
@@ -0,0 +1,82 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Bu kimlik bilgileri kayıtlarımızla eşleşmiyor.',
+    'throttle' => 'Çok fazla oturum açma denemesi. Lütfen :seconds saniye sonra tekrar deneyin.',
+
+    'full_name' => 'Ad Soyad',
+    'email' => 'E-Posta',
+    'password' => 'Parola',
+    'confirm_password' => 'Parolayı Onayla',
+    'remember_me' => 'Beni Hatırla',
+    'sign_in' => 'Giriş Yap',
+    'sign_out' => 'Çıkış Yap',
+    'register' => 'Kaydol',
+
+    'login' => [
+        'title' => 'Oturumunuzu başlatmak için giriş yapın',
+        'forgot_password' => 'Parolamı unuttum',
+        'register_membership' => 'Yeni bir üyelik kaydı oluşturun',
+    ],
+
+    'registration' => [
+        'title' => 'Yeni bir üyelik kaydı oluşturun',
+        'i_agree' => 'Kabul Ediyorum',
+        'terms' => 'Şartlar',
+        'have_membership' => 'Zaten bir üyeliğim var',
+    ],
+
+    'forgot_password' => [
+        'title' => 'Parolanızı sıfırlamak için e-posta girin',
+        'send_pwd_reset' => 'Parola Sıfırlama Bağlantısı Gönder',
+    ],
+
+    'reset_password' => [
+        'title' => 'Parolanızı sıfırlayın',
+        'reset_pwd_btn' => 'Parolayı Sıfırla',
+    ],
+
+    'confirm_passwords' => [
+        'title' => 'Devam etmeden önce lütfen parolanızı onaylayın.',
+        'forgot_your_password' => 'Parolanızı mı unuttunuz?',
+    ],
+
+    'verify_email' => [
+        'title' => 'E-posta adresinizi doğrulayın',
+        'success' => 'Yeni bir doğrulama bağlantısı e-posta adresinize gönderildi',
+        'notice' => 'Devam etmeden önce, doğrulama bağlantısı için lütfen e-postanızı kontrol edin. E-postayı almadıysanız,',
+        'another_req' => 'yeniden istemek için buraya tıklayın',
+    ],
+
+    'emails' => [
+        'password' => [
+            'reset_link' => 'Parolanızı sıfırlamak için buraya tıklayın',
+        ],
+    ],
+
+    'app' => [
+        'member_since' => 'Üye olduğu tarih',
+        'messages' => 'Mesajlar',
+        'settings' => 'Ayarlar',
+        'lock_account' => 'Hesabı Kitle',
+        'profile' => 'Profil',
+        'online' => 'Çevrimiçi',
+        'search' => 'Ara',
+        'create' => 'Oluştur',
+        'export' => 'Dışa Aktar',
+        'print' => 'Yazdır',
+        'reset' => 'Sıfırla',
+        'reload' => 'Yeniden Yükle',
+    ],
+];
---
+++ locale/tr/crud.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'add_new' => 'Yeni Ekle',
+    'cancel' => 'İptal',
+    'save' => 'Kaydet',
+    'edit' => 'Düzenle',
+    'detail' => 'Detay',
+    'back' => 'Geri',
+    'action' => 'İşlem',
+    'id' => 'Kimlik',
+    'created_at' => 'Oluşturulma Tarihi',
+    'updated_at' => 'Güncellenme Tarihi',
+    'deleted_at' => 'Silinme Tarihi',
+    'are_you_sure' => 'Emin misiniz?',
+];
